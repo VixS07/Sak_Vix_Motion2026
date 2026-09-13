d6edab88946e19c7bfd77b308d81057a922685d7
Vix <vix.sak@gmail.com>
2026-09-13 18:55:26 -0400

    made a ui system which allows you to draw a row of squares based on your input, was not able to impliment the already drawn lines deleting in the time frame.

---
+++ Sak_Vix_Motion2026/Assets/Scripts/Assignments/Week 1 Journal/RowGeneration.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RowGeneration : MonoBehaviour
+{
+    // Number of objects in the row
+    public int numberOfObjects; 
+
+    //base points of the square
+    public Vector2 point1 = new Vector2(1, 1);
+    public Vector2 point2 = new Vector2(1, -1);
+    public Vector2 point3 = new Vector2(-1, -1);
+    public Vector2 point4 = new Vector2(-1, 1);
+
+    //putting the points into an array so they can be looped through
+    public Vector2[] points;
+    public float time = 100f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        points = new Vector2[] { point1, point2, point3, point4 };
+    }
+
+    // Update is called once per frame
+    void Update() { 
+    
+            
+        
+    }
+
+    public void drawRow()
+    {
+        for (int j = 0; j < numberOfObjects; j++)
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                Vector2 pos = points[i];
+                Debug.DrawLine(pos, points[(i + 1) % points.Length], Color.purple, time);
+            }
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].x += 2; // Move the next square to the right
+            }
+        }
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i].x -= 2 * numberOfObjects; // Reset the position of the points for the next draw
+        }
+
+    }
+
+    public void changeNumInRow(string num)
+    {
+        //reference for changing a string into an int
+        //https://stackoverflow.com/questions/2344411/how-to-convert-string-to-integer-in-c-sharp
+        if (int.TryParse(num, out int newNum))
+        {
+            numberOfObjects = newNum;
+            Debug.Log("Number of objects in the row changed to: " + numberOfObjects);
+        }
+        else
+        {
+            Debug.LogError("Invalid input for number of objects: " + num);
+        }
+    }
+}
