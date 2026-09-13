bcfcf9236be073cd909911cbcf13f8b672c03bc4
Vix <vix.sak@gmail.com>
2026-09-13 13:08:10 -0400

    added comments to help with journal visuals

--- Sak_Vix_Motion2026/Assets/Scripts/Assignments/Week 1 Journal/SquareSpawner.cs
+++ Sak_Vix_Motion2026/Assets/Scripts/Assignments/Week 1 Journal/SquareSpawner.cs
@@ -10,8 +10,10 @@ public class SquareSpawner : MonoBehaviour
     public Vector2 point3 = new Vector2(-1, -1);
     public Vector2 point4 = new Vector2(-1, 1);
 
+    //putting the points into an array so they can be looped through
     public Vector2[] points;
 
+    //scale of the square when the mouse scroll wheel is used
     public float scale = 2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,7 +33,10 @@ public class SquareSpawner : MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         for(int i = 0; i < points.Length; i++)
         {
+            //adds the mouse position to the points of the square so it follows the mouse
             Vector2 pos = points[i] + mousePos;
+            //draws the square in grey
+            //dividing by points.length to make sure the last point connects to the first point
             Debug.DrawLine(pos, points[(i + 1) % points.Length] + mousePos, Color.grey);
         }
 
@@ -41,6 +46,7 @@ public class SquareSpawner : MonoBehaviour
             for (int i = 0; i < points.Length; i++)
             {
                 Vector2 pos = points[i] + mousePos;
+                //drawing it in white to differenciate this square for the one that follows the mouse
                 Debug.DrawLine(pos, points[(i + 1) % points.Length] + mousePos, Color.white, 1000f);
             }
 
@@ -48,7 +54,8 @@ public class SquareSpawner : MonoBehaviour
 
         //scaling the square down using the scroll wheel
         Vector2 scrollValue = Mouse.current.scroll.ReadValue();
-        if(scrollValue.y > 0)
+        //checking to see if the scroll wheel is being used to help with debugging
+        if (scrollValue.y > 0)
         {
             Debug.Log("mousescroll moved up");
             Debug.Log("scroll value: " + scrollValue.y);
@@ -60,9 +67,12 @@ public class SquareSpawner : MonoBehaviour
         }
         if(scrollValue.y != 0)
         {
+            //looping through the list
             for (int i = 0; i < points.Length; i++)
             {
-                if(i == 0)
+                //changing the addition/subtraction of the points based on the quadrant the point is in
+                //to make sure the square scales down correctly
+                if (i == 0)
                 {
                     points[0].y += (scale * scrollValue.y);
                     points[0].x += (scale * scrollValue.y);
