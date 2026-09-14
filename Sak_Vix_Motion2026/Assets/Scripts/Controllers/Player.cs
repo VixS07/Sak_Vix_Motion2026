62a58b2ceed6b53505dd9e638d7defebb45d17c7
Vix Sak <sakv@shernet.sheridancollege.ca>
2026-09-14 17:26:29 -0400

    edited the player script to spawn in a bomb object relative to the player in a specific offset.

---
+++ Sak_Vix_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public List<Transform> asteroidTransforms;
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public Transform bombsTransform;
+
+    void Update()
+    {
+        Vector2 offset = transform.position + Vector3.up;
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            SpawnBombAtOffSet(offset);
+        }
+
+        if (Keyboard.current.wKey.wasPressedThisFrame)
+        {
+
+        }
+    }
+
+    void SpawnBombAtOffSet(Vector2 inOffSet)
+    {
+        GameObject bomb =Instantiate(bombPrefab);
+        bomb.transform.position = inOffSet;
+    }
+
+    void PlayerJump(Vector2 movementAmount)
+    {
+        
+    }
+}
